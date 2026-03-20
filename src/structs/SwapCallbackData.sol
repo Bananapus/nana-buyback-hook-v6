34b5a6aa09ca89e6ac4e088d45130f81d4da2220
Jango <me.jango@protonmail.com>
2026-03-20 16:42:04 -0300

    Document swap callback struct

--- src/structs/SwapCallbackData.sol
+++ src/structs/SwapCallbackData.sol
@@ -4,6 +4,10 @@ pragma solidity 0.8.26;
 import {PoolKey} from "@uniswap/v4-core/src/types/PoolKey.sol";
 
 /// @notice Data passed through to the unlock callback.
+/// @custom:member key The pool key to swap against.
+/// @custom:member zeroForOne Whether the swap moves from `currency0` to `currency1`.
+/// @custom:member amountIn The exact amount of input tokens to swap.
+/// @custom:member minimumSwapAmountOut The minimum acceptable amount of output tokens.
 struct SwapCallbackData {
     PoolKey key;
     bool zeroForOne;
